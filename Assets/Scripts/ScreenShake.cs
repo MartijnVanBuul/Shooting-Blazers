a26612bb2c6c67e82c3f78ba172f5eb7ff8d6cf8
M. Fadl <m.fadl@live.nl>
2016-03-26 22:07:26 +0100

    Screenshake fixed

--- Assets/Scripts/ScreenShake.cs
+++ Assets/Scripts/ScreenShake.cs
@@ -5,20 +5,23 @@ public class ScreenShake : MonoBehaviour {
 
 	public float Intensity = 0.1f;
 	public float MaximumDeviation = 1.0f;
+	public bool debug = false;
 
 	float shakeTimeLeft = 0.0f;
 	Vector3 shakeCummulative = new Vector3();
 
 	// Use this for initialization
-	void Start () {
-		//ScreenShake(5.0f);
+	void Start () 
+	{
+		DoScreenShakeReset(); 
+		DoScreenShake(3.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		Vector3 cameraAdd = new Vector3();
-
+		
 		if (shakeTimeLeft > 0.0f)
 		{
 			shakeTimeLeft -= Time.deltaTime;
@@ -38,38 +41,47 @@ public class ScreenShake : MonoBehaviour {
 		}
 		else // Return to 0
 		{
+			shakeTimeLeft = 0.0f;
+
 			if (shakeCummulative.x > Intensity)
 				cameraAdd.x = -Intensity;
 			else if (shakeCummulative.x < -Intensity)
 				cameraAdd.x = Intensity;
 			else
-				cameraAdd.x = shakeCummulative.x;
+				cameraAdd.x = -shakeCummulative.x;
 
 			if (shakeCummulative.z > Intensity)
 				cameraAdd.z = -Intensity;
 			else if (shakeCummulative.z < -Intensity)
 				cameraAdd.z = Intensity;
 			else
-				cameraAdd.z = shakeCummulative.z;
+				cameraAdd.z = -shakeCummulative.z;
 		}
 
 		// Process position change
 		shakeCummulative += cameraAdd;
 		Camera.main.transform.position += cameraAdd;
+
+		if (debug)
+			Debug.Log("Time: " + shakeTimeLeft + " Shake: (" + cameraAdd.x + ", " + cameraAdd.z + "):  (" + shakeCummulative.x + ", " + shakeCummulative.z + ")");
 	}
 
-	/*
-	public void ScreenShake(float duration)
+
+	public void DoScreenShake(float duration)
 	{
-		Debug.Log("Shake for " + duration / 60.0f + " seconds.");
 		shakeTimeLeft = duration;
+
+		if (debug) 
+			Debug.Log("Shake for " + duration + " seconds.");
 	}
 
-	public void ScreenShakeReset()
+	public void DoScreenShakeReset()
 	{
-		Debug.Log("Shake reset");
 		shakeTimeLeft = 0;
 		Camera.main.transform.position -= shakeCummulative;
 		shakeCummulative = Vector3.zero;
-	}*/
+
+		if (debug)
+			Debug.Log("Shake reset");
+	}
 }
